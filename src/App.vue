68ccd68cff3eb51d4342d9570441469e395abdcb
fabd <fabrice.denis@gmail.com>
2021-03-25 18:00:19 +0100

    Add an optional <header> so the stand alone repo has a minimal layout
    - optional <header> section to spice up a bit visually
    - use a more generic title, to reduce initial setup for Diablo II
      mods, of course custom font could be used (maybe we'll add the
      exocet font later, not sure)
    - use .env settings for the game name & version displayed in header
    - clean up the stylesheet a bit, add a few comments
    
    Copyright Notice
    
    - the rune icon comes from a screenshot of Diablo II Resurrected, the
      artwork is Copyright BLIZZARD ENTERTAINMENT

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,34 @@
 <template>
-  <main class="rw-Main p-4 pl-8 flex max-w-[1000px]">
+  <div v-if="useLayoutHeader">
+    <!-- optional header to decorate the stand alone version -->
+    <header class="h-[106px] flex">
+      <div class="px-[20px] pt-[17px]">
+        <img
+          src="@/assets/images/logo-rune.png"
+          alt="Rune icon original art (c) BLIZZARD ENTERTAINMENT"
+          class="w-[69px] h-[67px]"
+        />
+      </div>
+      <div class="">
+        <h1 class="text-black text-[0px] mt-[19px] mb-[5px] w-[301px] h-[32px]">
+          <img
+            src="@/assets/images/logo-text-runewizard.png"
+            alt="Runewizard"
+            class="block w-full h-full"
+          />
+        </h1>
+        <span class="block text-lg text-[#afafaf]">
+          for <span class="text-[#d0d0d0] font-bold">{{ gameName }}</span>
+          <span class="ml-2 text-[#71B643]">{{ `v${gameVersion}` }}</span>
+        </span>
+      </div>
+    </header>
+
+    <!-- separator between header and main content, div simply extends to edges -->
+    <div class="rw-Header-goldBar mb-2"></div>
+  </div>
+
+  <main class="rw-Main p-4 pl-8 flex max-w-[1000px] mb-[15rem]">
     <div class="mr-8">
       <runes />
     </div>
@@ -21,6 +50,19 @@ export default defineComponent({
   components: {
     Runes,
     RunewordsTable,
+  },
+
+  data() {
+    return {
+      gameName: import.meta.env.VITE_GAME_NAME as string,
+      gameVersion: import.meta.env.VITE_GAME_VERSION as string,
+    };
+  },
+
+  computed: {
+    useLayoutHeader(): boolean {
+      return import.meta.env.VITE_LAYOUT_HEADER === "true";
+    },
   },
 });
 </script>
