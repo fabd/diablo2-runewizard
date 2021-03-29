fe8ee808920cb25f6c23d287d978ee8a6ed75a57
fabd <fabrice.denis@gmail.com>
2021-03-29 17:14:41 +0200

    (refactor): move the layout header to a separate component

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,5 @@
 <template>
-  <div v-if="useLayoutHeader">
-    <!-- optional header to decorate the stand alone version -->
-    <header class="h-[106px] flex">
-      <div class="px-[20px] pt-[17px]">
-        <img
-          src="@/assets/images/logo-rune.png"
-          alt="Rune icon original art (c) BLIZZARD ENTERTAINMENT"
-          class="w-[69px] h-[67px]"
-        />
-      </div>
-      <div class="">
-        <h1 class="text-black text-[0px] mt-[19px] mb-[5px] w-[301px] h-[32px]">
-          <img
-            src="@/assets/images/logo-text-runewizard.png"
-            alt="Runewizard"
-            class="block w-full h-full"
-          />
-        </h1>
-        <span class="block text-lg text-[#afafaf]">
-          for <span class="text-[#d0d0d0] font-bold">{{ gameName }}</span>
-          <span class="ml-2 text-[#71B643]">{{ `v${gameVersion}` }}</span>
-        </span>
-      </div>
-    </header>
-
-    <!-- separator between header and main content, div simply extends to edges -->
-    <div class="rw-Header-goldBar mb-2"></div>
-  </div>
+  <app-header v-if="useLayoutHeader" />
 
   <main class="rw-Main p-4 pl-8 flex max-w-[1000px] mb-[15rem]">
     <div class="mr-16">
@@ -41,6 +14,7 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 
+import AppHeader from "./components/AppHeader.vue";
 import Runes from "./components/Runes.vue";
 import RunewordsTable from "./components/RunewordsTable.vue";
 
@@ -48,15 +22,9 @@ export default defineComponent({
   name: "App",
 
   components: {
+    AppHeader,
     Runes,
     RunewordsTable,
-  },
-
-  data() {
-    return {
-      gameName: import.meta.env.VITE_GAME_NAME as string,
-      gameVersion: import.meta.env.VITE_GAME_VERSION as string,
-    };
   },
 
   computed: {
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,43 @@
+<template>
+  <!-- optional header to decorate the stand alone version -->
+  <header class="h-[106px] flex">
+    <div class="px-[20px] pt-[17px]">
+      <img
+        src="@/assets/images/logo-rune.png"
+        alt="Rune icon original art (c) BLIZZARD ENTERTAINMENT"
+        class="w-[69px] h-[67px]"
+      />
+    </div>
+    <div class="">
+      <h1 class="text-black text-[0px] mt-[19px] mb-[5px] w-[301px] h-[32px]">
+        <img
+          src="@/assets/images/logo-text-runewizard.png"
+          alt="Runewizard"
+          class="block w-full h-full"
+        />
+      </h1>
+      <span class="block text-lg text-[#afafaf]">
+        for <span class="text-[#d0d0d0] font-bold">{{ gameName }}</span>
+        <span class="ml-2 text-[#71B643]">{{ `v${gameVersion}` }}</span>
+      </span>
+    </div>
+  </header>
+
+  <!-- separator between header and main content, div simply extends to edges -->
+  <div class="rw-Header-goldBar mb-2"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "AppHeader",
+
+  data() {
+    return {
+      gameName: import.meta.env.VITE_GAME_NAME as string,
+      gameVersion: import.meta.env.VITE_GAME_VERSION as string,
+    };
+  },
+});
+</script>
