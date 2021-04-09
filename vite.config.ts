6a5fe10e08d4428dec7f523c9b747587a3984154
fabd <fabrice.denis@gmail.com>
2021-04-09 15:48:08 +0200

    (build): update `base` for easier Github Pages deployment
    I thought using a path in there would cause issue locally, but Vite
    handles it. So I may as well already set the value for publishing
    with gh-pages branch.
    
    The value being the path of the repo under: fabd.github.io/

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   //   - disable code splitting for css & js (simplifies next step)
   //   - to avoid CORS, inline the css & js files in the index.html
   // ---------------------------------------------------------------------------
-  // base: "./",
+  base: "/diablo2-runewizard/",
 
   build: {
     // ---------------------------------------------------------------------------
