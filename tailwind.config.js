fdb556339e66a77a82b0bd87a83574421651c972
fabd <fabrice.denis@gmail.com>
2022-04-28 18:28:35 +0200

    (tailwind): extend fontSize (remove line-height), add `leading-1`

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,10 +14,25 @@ module.exports = {
       },
     },
 
+    // redeclare to remove the baked-in line-height, and add `md` alias
+    fontSize: {
+      xs: "0.75rem", // 12px
+      sm: "0.875rem", // 14px
+      base: "1rem",
+      lg: "1.125rem", // 18px
+
+      // alias for `text-base`
+      md: "1rem", // 16px
+    },
+
     extend: {
       fontSize: {
         // alias for `text-base`
         md: "1rem",
+      },
+
+      lineHeight: {
+        1: 1, // alias for `leading-none`
       },
     },
   },
